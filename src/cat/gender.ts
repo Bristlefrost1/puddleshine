758a80b6061e3a446958a079283cf49b901b08bd
Bristlefrost <me@bristlefrost.net>
2024-08-27 19:45:31 +0000

    feat: add utility functions for pronouns

--- src/cat/gender.ts
+++ src/cat/gender.ts
@@ -10,4 +10,78 @@ enum KitGender {
 	Other = '',
 }
 
-export { Gender, KitGender };
+type Pronouns = {
+	subject: string;
+	subjectU: string;
+
+	object: string;
+	objectU: string;
+
+	possessive: string;
+	possessiveU: string;
+
+	reflexive: string;
+	reflexiveU: string;
+};
+
+const nonBinary: Pronouns = {
+	subject: 'they',
+	subjectU: 'They',
+
+	object: 'them',
+	objectU: 'Them',
+
+	possessive: 'their',
+	possessiveU: 'Their',
+
+	reflexive: 'themselves',
+	reflexiveU: 'Themselves',
+};
+
+const masculine: Pronouns = {
+	subject: 'he',
+	subjectU: 'He',
+
+	object: 'him',
+	objectU: 'him',
+
+	possessive: 'his',
+	possessiveU: 'his',
+
+	reflexive: 'himself',
+	reflexiveU: 'himself',
+};
+
+const feminine: Pronouns = {
+	subject: 'she',
+	subjectU: 'She',
+
+	object: 'her',
+	objectU: 'Her',
+
+	possessive: 'her',
+	possessiveU: 'Her',
+
+	reflexive: 'herself',
+	reflexiveU: 'Herself',
+};
+
+function getPronouns(gender?: Gender | KitGender): Pronouns {
+	if (!gender) return nonBinary;
+
+	switch (gender) {
+		case Gender.Tom:
+		case KitGender.TomKit:
+			return masculine;
+
+		case Gender.SheCat:
+		case KitGender.SheKit:
+			return feminine;
+
+		default:
+			return nonBinary;
+	}
+}
+
+export { Gender, KitGender, getPronouns };
+export type { Pronouns };
