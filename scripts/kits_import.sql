cac484c84ccd0cf99493f30583dffa29e6b3e165
Bristlefrost <me@bristlefrost.net>
2024-09-06 18:18:01 +0000

    scripts: add kit import script

--- scripts/kits_import.sql
+++ scripts/kits_import.sql
@@ -1,10 +1,140 @@
-INSERT INTO nursery_kits (uuid, nursery_uuid, name_prefix, gender, age_moons, age_updated, bred_at, pelt, eyes, health, health_updated, hunger, hunger_updated, bond, bond_updated, temperature, temperature_updated, events) VALUES ('72b93ee0-6cd5-4909-9c22-6b847709c321', '4e7aee49-a27d-4c7a-a86b-25fa4c7302ea', 'Splash', 'Tom-kit', 5.57, IMPORT_TIME, IMPORT_TIME, '{"type":"SolidColor","color":"Blue"}', '{"type":"Normal","eyeColor":"Yellow"}', 1, IMPORT_TIME, 0.546, IMPORT_TIME, 0.999, IMPORT_TIME, 38.3, IMPORT_TIME, '[]');
-INSERT INTO nursery_kits (uuid, nursery_uuid, name_prefix, gender, age_moons, age_updated, bred_at, pelt, eyes, health, health_updated, hunger, hunger_updated, bond, bond_updated, temperature, temperature_updated, events) VALUES ('3fd93cc1-a953-4927-999f-48bc2a31fb87', '4e7aee49-a27d-4c7a-a86b-25fa4c7302ea', 'Swamp', 'Tom-kit', 5.56, IMPORT_TIME, IMPORT_TIME, '{"type":"SolidColor","color":"Brown"}', '{"type":"Normal","eyeColor":"Green"}', 1, IMPORT_TIME, 0.709, IMPORT_TIME, 0.997, IMPORT_TIME, 38.3, IMPORT_TIME, '[]');
-INSERT INTO nursery_kits (uuid, nursery_uuid, name_prefix, gender, age_moons, age_updated, bred_at, pelt, eyes, health, health_updated, hunger, hunger_updated, bond, bond_updated, temperature, temperature_updated, events) VALUES ('5de5ed1a-e2a0-4443-9da5-4a74d9c2fbdb', '4e7aee49-a27d-4c7a-a86b-25fa4c7302ea', 'Hope', 'She-kit', 5.33, IMPORT_TIME, IMPORT_TIME, '{"type":"SolidColor","color":"Cinnamon"}', '{"type":"Normal","eyeColor":"Blue"}', 1, IMPORT_TIME, 0.813, IMPORT_TIME, 1, IMPORT_TIME, 38.3, IMPORT_TIME, '[]');
-INSERT INTO nursery_kits (uuid, nursery_uuid, name_prefix, gender, age_moons, age_updated, bred_at, pelt, eyes, health, health_updated, hunger, hunger_updated, bond, bond_updated, temperature, temperature_updated, events) VALUES ('f45e0d5a-9d6e-44ad-a990-38b76ca89319', '4e7aee49-a27d-4c7a-a86b-25fa4c7302ea', 'Wind', 'Tom-kit', 5.01, IMPORT_TIME, IMPORT_TIME, '{"type":"SolidColor","color":"Red"}', '{"type":"Normal","eyeColor":"Blue"}', 1, IMPORT_TIME, 0.667, IMPORT_TIME, 1, IMPORT_TIME, 38.3, IMPORT_TIME, '[]');
-INSERT INTO nursery_kits (uuid, nursery_uuid, name_prefix, gender, age_moons, age_updated, bred_at, pelt, eyes, health, health_updated, hunger, hunger_updated, bond, bond_updated, temperature, temperature_updated, events) VALUES ('17ba0e66-ef2c-4153-952f-5581b1002919', '4e7aee49-a27d-4c7a-a86b-25fa4c7302ea', 'Stolas', 'Tom-kit', 4.35, IMPORT_TIME, IMPORT_TIME, '{"type":"SolidColor","color":"Blue"}', '{"type":"Normal","eyeColor":"Amber"}', 1, IMPORT_TIME, 0.483, IMPORT_TIME, 0.998, IMPORT_TIME, 38.3, IMPORT_TIME, '[]');
-INSERT INTO nursery_kits (uuid, nursery_uuid, name_prefix, gender, age_moons, age_updated, bred_at, pelt, eyes, health, health_updated, hunger, hunger_updated, bond, bond_updated, temperature, temperature_updated, events) VALUES ('601f6a2c-80aa-4b7b-9c3d-bfdd7a598f86', '4e7aee49-a27d-4c7a-a86b-25fa4c7302ea', 'Feather', 'She-kit', 5.67, IMPORT_TIME, IMPORT_TIME, '{"type":"SolidColor","color":"Black"}', '{"type":"Normal","eyeColor":"Amber"}', 1, IMPORT_TIME, 0.573, IMPORT_TIME, 0.999, IMPORT_TIME, 38.3, IMPORT_TIME, '[]');
-INSERT INTO nursery_kits (uuid, nursery_uuid, name_prefix, gender, age_moons, age_updated, bred_at, pelt, eyes, health, health_updated, hunger, hunger_updated, bond, bond_updated, temperature, temperature_updated, events) VALUES ('a01dff42-3034-48f9-8125-582cedc2e809', '4e7aee49-a27d-4c7a-a86b-25fa4c7302ea', 'Shadow', 'Tom-kit', 4.69, IMPORT_TIME, IMPORT_TIME, '{"type":"SolidColor","color":"Brown"}', '{"type":"Normal","eyeColor":"Golden"}', 1, IMPORT_TIME, 0.591, IMPORT_TIME, 0.999, IMPORT_TIME, 38.3, IMPORT_TIME, '[]');
-INSERT INTO nursery_kits (uuid, nursery_uuid, name_prefix, gender, age_moons, age_updated, bred_at, pelt, eyes, health, health_updated, hunger, hunger_updated, bond, bond_updated, temperature, temperature_updated, events) VALUES ('9cbb16e8-c74c-435e-9aaf-7d485558d215', '4e7aee49-a27d-4c7a-a86b-25fa4c7302ea', 'Night', 'Tom-kit', 4.69, IMPORT_TIME, IMPORT_TIME, '{"type":"SolidColor","color":"Silver"}', '{"type":"Normal","eyeColor":"Green"}', 1, IMPORT_TIME, 0.484, IMPORT_TIME, 0.998, IMPORT_TIME, 38.3, IMPORT_TIME, '[]');
-INSERT INTO nursery_kits (uuid, nursery_uuid, name_prefix, gender, age_moons, age_updated, bred_at, pelt, eyes, health, health_updated, hunger, hunger_updated, bond, bond_updated, temperature, temperature_updated, events) VALUES ('5066bdfb-db36-4018-9d53-2fb926c6da53', '4e7aee49-a27d-4c7a-a86b-25fa4c7302ea', 'Sky', 'She-kit', 4.55, IMPORT_TIME, IMPORT_TIME, '{"type":"SolidColor","color":"Brown"}', '{"type":"Normal","eyeColor":"Copper"}', 1, IMPORT_TIME, 0.683, IMPORT_TIME, 1, IMPORT_TIME, 38.3, IMPORT_TIME, '[]');
-INSERT INTO nursery_kits (uuid, nursery_uuid, name_prefix, gender, age_moons, age_updated, bred_at, pelt, eyes, health, health_updated, hunger, hunger_updated, bond, bond_updated, temperature, temperature_updated, events) VALUES ('8137263e-062a-4e8f-b6fa-49afc41f2e68', '4e7aee49-a27d-4c7a-a86b-25fa4c7302ea', 'Leaf', 'Tom-kit', 0.87, IMPORT_TIME, IMPORT_TIME, '{"type":"SolidColor","color":"Cinnamon"}', '{"type":"Normal","eyeColor":"Golden"}', 1, IMPORT_TIME, 0.552, IMPORT_TIME, 0.308, IMPORT_TIME, 38.3, IMPORT_TIME, '[]');
+INSERT INTO nursery_kits (uuid, nursery_uuid, name_prefix, gender, age_moons, age_updated, bred_at, pelt, eyes, health, health_updated, hunger, hunger_updated, bond, bond_updated, temperature, temperature_updated, events) VALUES (
+	'a5d0bea2-f00c-46e7-a00b-8c04d586bf4d',
+	'76913acc-a2ea-4dde-966e-ee744d47c320',
+	'Apple',
+	'She-kit',
+	5.7, -- Age moons
+	IMPORT_TIME,
+	IMPORT_TIME,
+	'{"type":"SolidColor","color":"Gray"}',
+	'{"type":"Normal","eyeColor":"Green"}',
+	0.883, -- Health
+	IMPORT_TIME,
+	0.585, -- Hunger
+	IMPORT_TIME,
+	0.373, -- Bond
+	IMPORT_TIME,
+	39.2, -- Temperature
+	IMPORT_TIME,
+	'[]'
+);
+INSERT INTO nursery_kits (uuid, nursery_uuid, name_prefix, gender, age_moons, age_updated, bred_at, pelt, eyes, health, health_updated, hunger, hunger_updated, bond, bond_updated, temperature, temperature_updated, events) VALUES (
+	'd5ac25ee-dea7-42f4-abbf-dc73753bcdfa',
+	'76913acc-a2ea-4dde-966e-ee744d47c320',
+	'Thorn',
+	'Tom-kit',
+	4.97, -- Age moons
+	IMPORT_TIME,
+	IMPORT_TIME,
+	'{"type":"SolidColor","color":"Blue"}',
+	'{"type":"Normal","eyeColor":"Aqua"}',
+	0.958, -- Health
+	IMPORT_TIME,
+	0.582, -- Hunger
+	IMPORT_TIME,
+	0.5, -- Bond
+	IMPORT_TIME,
+	39.2, -- Temperature
+	IMPORT_TIME,
+	'[]'
+);
+INSERT INTO nursery_kits (uuid, nursery_uuid, name_prefix, gender, age_moons, age_updated, bred_at, pelt, eyes, health, health_updated, hunger, hunger_updated, bond, bond_updated, temperature, temperature_updated, events) VALUES (
+	'71a35402-4652-4767-ac22-34062278c3c1',
+	'76913acc-a2ea-4dde-966e-ee744d47c320',
+	'Breeze',
+	'She-kit',
+	3.97, -- Age moons
+	IMPORT_TIME,
+	IMPORT_TIME,
+	'{"type":"SolidColor","color":"Brown"}',
+	'{"type":"Normal","eyeColor":"Copper"}',
+	0.764, -- Health
+	IMPORT_TIME,
+	0.645, -- Hunger
+	IMPORT_TIME,
+	0.593, -- Bond
+	IMPORT_TIME,
+	39.2, -- Temperature
+	IMPORT_TIME,
+	'[]'
+);
+INSERT INTO nursery_kits (uuid, nursery_uuid, name_prefix, gender, age_moons, age_updated, bred_at, pelt, eyes, health, health_updated, hunger, hunger_updated, bond, bond_updated, temperature, temperature_updated, events) VALUES (
+	'f2473bb3-3d1b-4935-ac0c-dac503230cb6',
+	'76913acc-a2ea-4dde-966e-ee744d47c320',
+	'Bristle',
+	'She-kit',
+	5.35, -- Age moons
+	IMPORT_TIME,
+	IMPORT_TIME,
+	'{"type":"SolidColor","color":"Black"}',
+	'{"type":"Normal","eyeColor":"Orange"}',
+	0.849, -- Health
+	IMPORT_TIME,
+	0.186, -- Hunger
+	IMPORT_TIME,
+	0.155, -- Bond
+	IMPORT_TIME,
+	39.2, -- Temperature
+	IMPORT_TIME,
+	'[]'
+);
+INSERT INTO nursery_kits (uuid, nursery_uuid, name_prefix, gender, age_moons, age_updated, bred_at, pelt, eyes, health, health_updated, hunger, hunger_updated, bond, bond_updated, temperature, temperature_updated, events) VALUES (
+	'c76a9abf-210f-456a-9717-9989f2a4faa0',
+	'76913acc-a2ea-4dde-966e-ee744d47c320',
+	'Spotted',
+	'Tom-kit',
+	2.12, -- Age moons
+	IMPORT_TIME,
+	IMPORT_TIME,
+	'{"type":"SolidColor","color":"White"}',
+	'{"type":"Normal","eyeColor":"Orange"}',
+	0.826, -- Health
+	IMPORT_TIME,
+	0.328, -- Hunger
+	IMPORT_TIME,
+	0.462, -- Bond
+	IMPORT_TIME,
+	39.1, -- Temperature
+	IMPORT_TIME,
+	'[]'
+);
+INSERT INTO nursery_kits (uuid, nursery_uuid, name_prefix, gender, age_moons, age_updated, bred_at, pelt, eyes, health, health_updated, hunger, hunger_updated, bond, bond_updated, temperature, temperature_updated, events) VALUES (
+	'31c977bb-0392-4c7b-ad2b-0413e3846209',
+	'76913acc-a2ea-4dde-966e-ee744d47c320',
+	'Duck',
+	'Tom-kit',
+	1.43, -- Age moons
+	IMPORT_TIME,
+	IMPORT_TIME,
+	'{"type":"SolidColor","color":"Gray"}',
+	'{"type":"Normal","eyeColor":"Amber"}',
+	0.854, -- Health
+	IMPORT_TIME,
+	0.578, -- Hunger
+	IMPORT_TIME,
+	0.518, -- Bond
+	IMPORT_TIME,
+	39.1, -- Temperature
+	IMPORT_TIME,
+	'[]'
+);
+INSERT INTO nursery_kits (uuid, nursery_uuid, name_prefix, gender, age_moons, age_updated, bred_at, pelt, eyes, health, health_updated, hunger, hunger_updated, bond, bond_updated, temperature, temperature_updated, events) VALUES (
+	'35f4c048-c6d8-425c-9766-f6102bb18c99',
+	'76913acc-a2ea-4dde-966e-ee744d47c320',
+	'Pigeon',
+	'She-kit',
+	1.43, -- Age moons
+	IMPORT_TIME,
+	IMPORT_TIME,
+	'{"type":"SolidColor","color":"Amber"}',
+	'{"type":"Normal","eyeColor":"Emerald"}',
+	1, -- Health
+	IMPORT_TIME,
+	0.327, -- Hunger
+	IMPORT_TIME,
+	0.606, -- Bond
+	IMPORT_TIME,
+	39.1, -- Temperature
+	IMPORT_TIME,
+	'[]'
+);
